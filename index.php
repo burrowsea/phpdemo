684da9525ba65d23d0b0f58ceeb6d5941fc8635e
burrows.ea <burrows.ea@oundleschool.org.uk>
2024-10-15 09:54:42 +0100

    flie in

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+</head>
+<body>
+    <br>
+    <h1>Hello</h1>
+    
+    <p>&nbspoooooooooooo&nbsp<br>&nbspo &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspo<br>&nbspo&nbsp&nbsp#&nbsp&nbsp    #&nbsp&nbsp  o&nbsp<br>&nbspo &nbsp&nbsp    #&nbsp&nbsp&nbsp&nbsp    o<br>&nbspo  &nbsp#&nbsp&nbsp    #&nbsp&nbsp  o<br>&nbspo  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp        o<br>&nbspoooooooooooo</p>
+    
+</body>
+</html>
